c958e4518bd80fa3503d0bcaddd72959859a0bb9
mappingpln <absenmapping@gmail.com>
2026-09-16 14:52:27 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,7 @@
-self.addEventListener('fetch', function(e) {
+const CACHE_NAME = 'hris-absen-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./', './index.html', './manifest.json'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
