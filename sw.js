8c24f59126e34db1b086773fdc665470055d930c
mappingpln <absenmapping@gmail.com>
2026-09-17 09:05:44 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,42 @@
-const CACHE_NAME = 'hris-absen-v1';
+const CACHE_VERSION = 'v3';
+const CACHE_NAME = 'hris-absen-' + CACHE_VERSION;
+const CORE_ASSETS = ['./', './index.html', './manifest.json'];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./', './index.html', './manifest.json'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(c => c.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  const isShell = e.request.mode === 'navigate' || CORE_ASSETS.some(a => url.pathname.endsWith(a.replace('./', '')));
+  if (isShell) {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
