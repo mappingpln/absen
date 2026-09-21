b22e43e93bcaaee438176cd20a8d91551d060b94
mappingpln <absenmapping@gmail.com>
2026-09-22 05:54:25 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v14';
+const CACHE_VERSION = 'v15';
 const CACHE_NAME = 'hris-absen-' + CACHE_VERSION;
 const CDN_CACHE = 'hris-cdn-' + CACHE_VERSION;
 const CORE_ASSETS = ['./', './index.html', './manifest.json'];
